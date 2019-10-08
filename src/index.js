c55c94c226bc4e098d83ab81612429b7708e5ae7
Ivan Aksamentov <9403403+ivan-aksamentov@users.noreply.github.com>
2019-10-08 22:32:09 +0200

    fix: add `ViewportOverlay` as a named export (#11)
    Adds `ViewportOverlay` as a named export of the package, additionally to the default export of `CornerstoneViewport`.
    
    Explicit export would allow downstream users to import and use `ViewportOverlay` separately in their projects (e.g. in OHIF/Viewers extensions or customized overlays).

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import './metadataProvider.js';
 import CornerstoneViewport from './CornerstoneViewport/CornerstoneViewport.js';
 
+import ViewportOverlay from './ViewportOverlay/ViewportOverlay';
+
+export { ViewportOverlay };
+
 export default CornerstoneViewport;
