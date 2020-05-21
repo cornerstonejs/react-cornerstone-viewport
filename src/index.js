0fcdfefcbc114423511692d237b34edaef9ac315
James Petts <jamesapetts@gmail.com>
2020-05-21 12:12:38 +0100

    fix: trigger release of recent changes.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './metadataProvider.js';
 import CornerstoneViewport from './CornerstoneViewport/CornerstoneViewport.js';
-
 import ViewportOverlay from './ViewportOverlay/ViewportOverlay';
 
 export { ViewportOverlay };
